be06a8a62446c7396368c7b92f33796f43b95cc7
Anastasia Filatova <56803387+BloodInfection@users.noreply.github.com>
2021-09-13 15:20:00 +0300

    update

--- dz1/main.go
+++ dz1/main.go
@@ -14,10 +14,14 @@ import (
 	fmt.Println("nothing here yet")
 }*/
 
+func test() string {
+	return "massive"
+}
+
 func main() {
 
 	out := os.Stdout
-	path := os.Args[1] /* путь = второму аргументу(например точка это путь к текущему файлу) */
+	path := os.Args[1]
 	/* будем печатать файлы или нет */
 
 	if !(len(os.Args) == 2 || len(os.Args) == 3) {
@@ -26,16 +30,18 @@ func main() {
 	printFiles := len(os.Args) == 3 && os.Args[2] == "-f" /*true or false */
 	fmt.Println(path, printFiles, out)
 	dirTree(out, path, printFiles)
+
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) (err error) {
 	/* ShowFiles := flag.Bool("f", false, "shows files")
 	flag.Parse()  флаг для вывода не только папок, но и их содержимого  */
-
-	var MassiveOfFiles []string              // заносим названия ФАЙЛОВ в директории
-	var MassiveOfDirs []string               /* заносим названия ПАПОК в директории */
-	DirsAndFiles, err := ioutil.ReadDir(".") /* пояснение внизу */
-	if err != nil {                          /* какая то ошибка при чтении директории */
+	/* var sliceForGraffiti []bool */
+	var MassiveOfFiles []string // заносим названия ФАЙЛОВ в директории
+	var MassiveOfDirs []string
+	/* var MassiveOfDirs []string   */        /* заносим названия ПАПОК в директории */
+	DirsAndFiles, err := ioutil.ReadDir(path) /* пояснение внизу */
+	if err != nil {
 		log.Fatal(err)
 	}
 
@@ -49,8 +55,17 @@ func dirTree(out io.Writer, path string, printFiles bool) (err error) {
 
 	}
 	sort.Strings(MassiveOfFiles)
-	fmt.Println(MassiveOfFiles)
-	fmt.Println(MassiveOfDirs)
+
+	fmt.Println("файлы ", MassiveOfFiles)
+	fmt.Println("папки ", MassiveOfDirs)
+
+	for _, dirName := range MassiveOfDirs {
+		println("содержимое папки ", dirName)
+		dirTree(out, path+"/"+dirName, printFiles) /* выполняется то же самое что с текущей директорией, только уже со следующей по порядку */
+		/* fmt.Println("подпапка " + dirName + " это " + path + "/" + dirName) */
+
+	}
+
 	return nil
 }
 
@@ -58,13 +73,9 @@ func dirTree(out io.Writer, path string, printFiles bool) (err error) {
 Т.е. у вас должен быть код который отсортирует уровень.
 Смотрите для этого пакет sort.
 Это самая частая причина непрохождения тестов. Тесты запускаются в среде linux. В задании есть докер-файл для тестов ровно в тех же условиях, он сразу выявит все проблемы.
-
 https://pkg.go.dev/io/ioutil#example-ReadDir
-
 STRCONV
 i, err := strconv.Atoi("-42") (string to int)
 s := strconv.Itoa(-42) (int to string)
-
 " ("+strconv.Itoa(int(file.Size()))+"b)") - если делать без strconv, выдает ошибку, несовместимые типы данных стринг и инт
-
 */
